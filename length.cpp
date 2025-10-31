f81183ffdc9cf2d234173fc940ab6e2b099367e9
Putri Liansari <putrilianasari28@gmail.com>
2025-10-31 13:42:42 +0700

    Menghitung string

---
+++ length.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string kalimat = "Hi Min Yoongi!"; 
+    int panjang1 = kalimat.length();
+    cout << "Panjang string : " << panjang1 << endl;
+
+    return 0;
+}
